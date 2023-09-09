b5ee626f9056a39549f5995107c2add73bb77ea4
NeoTryn <eidlerfelix@gmail.com>
2023-09-09 10:52:56 +0200

    test files added

---
+++ aufgabe1/helloWorld.java
@@ -0,0 +1,9 @@
+class helloWorld {
+				public static void main(String[] args) {
+								System.out.println(add(3, 4));
+				}
+
+				public static int add(int a, int b) {
+								return a + b;
+				}
+}
